a785433861b33c77361c5273a5da0a08375a63d2
Mirae Yoo <miraeyoo@Miraeui-MacBookAir.local>
2023-12-23 13:25:30 +0900

    2nd Add Netlify functions

--- functions/hello.js
+++ functions/hello.js
@@ -2,9 +2,9 @@ exports.handler = async function (event, context) {
   return {
     statusCode: 200,
     body: JSON.stringify({
-      name: 'heropy',
-      age: 85,
-      email: 'thecosne@gmail.com'
+      name: 'miraezzang',
+      age: 40,
+      email: 'dnl.mgowij@gmail.com'
     })
   }
 }
